5c954fbd5e15bdb5ae7247f3651a9027b9cd45b0
ShanssvM <shanthissv@yahoo.com>
2025-05-06 20:47:28 -0400

    Create network-data.js

---
+++ network-data.js
@@ -0,0 +1,714 @@
+// Pre-processed network data from Excel job Opp_10.xlsx
+// Generated on May 6, 2025
+// This file contains network data structured for D3.js visualization
+// with nodes (jobs, strengths, interests, education) and their connections
+
+const NETWORK_DATA = {
+  "nodes": [
+    {
+      "id": 0,
+      "name": "Software Engineer",
+      "type": "job",
+      "industry": "Business",
+      "salary": "$120k+",
+      "workMode": "On-site",
+      "sourceLink": "https://www.linkedin.com/jobs",
+      "connections": 15
+    },
+    {
+      "id": 1,
+      "name": "Data Scientist",
+      "type": "job",
+      "industry": "Business",
+      "salary": "$120k+",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.glassdoor.com",
+      "connections": 15
+    },
+    {
+      "id": 2,
+      "name": "Project Manager",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$40k-$60k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.flexjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 3,
+      "name": "UX Designer",
+      "type": "job",
+      "industry": "Technology",
+      "salary": "$120k+",
+      "workMode": "On-site",
+      "sourceLink": "https://www.glassdoor.com",
+      "connections": 15
+    },
+    {
+      "id": 4,
+      "name": "Digital Marketer",
+      "type": "job",
+      "industry": "Technology",
+      "salary": "$80k-$100k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.glassdoor.com",
+      "connections": 15
+    },
+    {
+      "id": 5,
+      "name": "Financial Analyst",
+      "type": "job",
+      "industry": "Business",
+      "salary": "$80k-$100k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.flexjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 6,
+      "name": "Graphic Designer",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$60k-$80k",
+      "workMode": "Remote",
+      "sourceLink": "https://www.flexjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 7,
+      "name": "HR Specialist",
+      "type": "job",
+      "industry": "Business",
+      "salary": "$60k-$80k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.indeed.com",
+      "connections": 15
+    },
+    {
+      "id": 8,
+      "name": "Operations Manager",
+      "type": "job",
+      "industry": "Business",
+      "salary": "$100k-$120k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.indeed.com",
+      "connections": 15
+    },
+    {
+      "id": 9,
+      "name": "Content Writer",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$60k-$80k",
+      "workMode": "Remote",
+      "sourceLink": "https://www.upwork.com",
+      "connections": 15
+    },
+    {
+      "id": 10,
+      "name": "Product Manager",
+      "type": "job",
+      "industry": "Technology",
+      "salary": "$100k-$120k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.linkedin.com/jobs",
+      "connections": 15
+    },
+    {
+      "id": 11,
+      "name": "DevOps Engineer",
+      "type": "job",
+      "industry": "Technology",
+      "salary": "$120k+",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.linkedin.com/jobs",
+      "connections": 15
+    },
+    {
+      "id": 12,
+      "name": "Customer Service Rep",
+      "type": "job",
+      "industry": "Business",
+      "salary": "$40k-$60k",
+      "workMode": "Remote",
+      "sourceLink": "https://www.indeed.com",
+      "connections": 15
+    },
+    {
+      "id": 13,
+      "name": "Sales Representative",
+      "type": "job",
+      "industry": "Business",
+      "salary": "$60k-$80k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.glassdoor.com",
+      "connections": 15
+    },
+    {
+      "id": 14,
+      "name": "Business Analyst",
+      "type": "job",
+      "industry": "Business",
+      "salary": "$80k-$100k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.linkedin.com/jobs",
+      "connections": 15
+    },
+    {
+      "id": 15,
+      "name": "Marketing Manager",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$100k-$120k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.glassdoor.com",
+      "connections": 15
+    },
+    {
+      "id": 16,
+      "name": "Social Media Manager",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$60k-$80k",
+      "workMode": "Remote",
+      "sourceLink": "https://www.upwork.com",
+      "connections": 15
+    },
+    {
+      "id": 17,
+      "name": "Web Developer",
+      "type": "job",
+      "industry": "Technology",
+      "salary": "$80k-$100k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.linkedin.com/jobs",
+      "connections": 15
+    },
+    {
+      "id": 18,
+      "name": "Systems Administrator",
+      "type": "job",
+      "industry": "Technology",
+      "salary": "$80k-$100k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.dice.com",
+      "connections": 15
+    },
+    {
+      "id": 19,
+      "name": "Research Scientist",
+      "type": "job",
+      "industry": "Technology",
+      "salary": "$100k-$120k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.linkedin.com/jobs",
+      "connections": 15
+    },
+    {
+      "id": 20,
+      "name": "Teacher",
+      "type": "job",
+      "industry": "Education",
+      "salary": "$40k-$60k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.teacherjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 21,
+      "name": "Nurse",
+      "type": "job",
+      "industry": "Health & Wellness",
+      "salary": "$60k-$80k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.healthjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 22,
+      "name": "Physical Therapist",
+      "type": "job",
+      "industry": "Health & Wellness",
+      "salary": "$80k-$100k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.healthjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 23,
+      "name": "Counselor",
+      "type": "job",
+      "industry": "Health & Wellness",
+      "salary": "$60k-$80k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.indeed.com",
+      "connections": 15
+    },
+    {
+      "id": 24,
+      "name": "Non-profit Coordinator",
+      "type": "job",
+      "industry": "Public Services",
+      "salary": "$40k-$60k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.idealist.org",
+      "connections": 15
+    },
+    {
+      "id": 25,
+      "name": "Environmental Scientist",
+      "type": "job",
+      "industry": "Environmental",
+      "salary": "$80k-$100k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.environmentaljobs.com",
+      "connections": 15
+    },
+    {
+      "id": 26,
+      "name": "Urban Planner",
+      "type": "job",
+      "industry": "Public Services",
+      "salary": "$60k-$80k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.governmentjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 27,
+      "name": "Librarian",
+      "type": "job",
+      "industry": "Education",
+      "salary": "$40k-$60k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.libjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 28,
+      "name": "Museum Curator",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$60k-$80k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.museumjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 29,
+      "name": "Journalist",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$60k-$80k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.mediabistro.com",
+      "connections": 15
+    },
+    {
+      "id": 30,
+      "name": "Event Planner",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$60k-$80k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.indeed.com",
+      "connections": 15
+    },
+    {
+      "id": 31,
+      "name": "Chef",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$60k-$80k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.culinaryagents.com",
+      "connections": 15
+    },
+    {
+      "id": 32,
+      "name": "Photographer",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$60k-$80k",
+      "workMode": "Remote",
+      "sourceLink": "https://www.upwork.com",
+      "connections": 15
+    },
+    {
+      "id": 33,
+      "name": "Interior Designer",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$80k-$100k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.creativehotlist.com",
+      "connections": 15
+    },
+    {
+      "id": 34,
+      "name": "Fashion Designer",
+      "type": "job",
+      "industry": "Creative Arts",
+      "salary": "$80k-$100k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.fashionjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 35,
+      "name": "Psychologist",
+      "type": "job",
+      "industry": "Health & Wellness",
+      "salary": "$100k-$120k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.psychjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 36,
+      "name": "Personal Trainer",
+      "type": "job",
+      "industry": "Health & Wellness",
+      "salary": "$40k-$60k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.indeed.com",
+      "connections": 15
+    },
+    {
+      "id": 37,
+      "name": "Yoga Instructor",
+      "type": "job",
+      "industry": "Health & Wellness",
+      "salary": "$40k-$60k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.mindbodyonline.com",
+      "connections": 15
+    },
+    {
+      "id": 38,
+      "name": "Park Ranger",
+      "type": "job",
+      "industry": "Environmental",
+      "salary": "$40k-$60k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.usajobs.gov",
+      "connections": 15
+    },
+    {
+      "id": 39,
+      "name": "Conservation Scientist",
+      "type": "job",
+      "industry": "Environmental",
+      "salary": "$60k-$80k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.conservation-careers.com",
+      "connections": 15
+    },
+    {
+      "id": 40,
+      "name": "Sustainability Consultant",
+      "type": "job",
+      "industry": "Environmental",
+      "salary": "$80k-$100k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.linkedin.com/jobs",
+      "connections": 15
+    },
+    {
+      "id": 41,
+      "name": "Community Organizer",
+      "type": "job",
+      "industry": "Public Services",
+      "salary": "$40k-$60k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.idealist.org",
+      "connections": 15
+    },
+    {
+      "id": 42,
+      "name": "Social Worker",
+      "type": "job",
+      "industry": "Public Services",
+      "salary": "$40k-$60k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.socialservice.com",
+      "connections": 15
+    },
+    {
+      "id": 43,
+      "name": "Policy Analyst",
+      "type": "job",
+      "industry": "Public Services",
+      "salary": "$80k-$100k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.governmentjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 44,
+      "name": "Career Counselor",
+      "type": "job",
+      "industry": "Education",
+      "salary": "$60k-$80k",
+      "workMode": "Hybrid",
+      "sourceLink": "https://www.higheredjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 45,
+      "name": "College Professor",
+      "type": "job",
+      "industry": "Education",
+      "salary": "$80k-$100k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.higheredjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 46,
+      "name": "Academic Researcher",
+      "type": "job",
+      "industry": "Education",
+      "salary": "$60k-$80k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.higheredjobs.com",
+      "connections": 15
+    },
+    {
+      "id": 47,
+      "name": "Curriculum Developer",
+      "type": "job",
+      "industry": "Education",
+      "salary": "$60k-$80k",
+      "workMode": "Remote",
+      "sourceLink": "https://www.edjoin.org",
+      "connections": 15
+    },
+    {
+      "id": 48,
+      "name": "Online Tutor",
+      "type": "job",
+      "industry": "Education",
+      "salary": "$40k-$60k",
+      "workMode": "Remote",
+      "sourceLink": "https://www.tutor.com",
+      "connections": 15
+    },
+    {
+      "id": 49,
+      "name": "Educational Administrator",
+      "type": "job",
+      "industry": "Education",
+      "salary": "$80k-$100k",
+      "workMode": "On-site",
+      "sourceLink": "https://www.edjoin.org",
+      "connections": 15
+    },
+    {
+      "id": 50,
+      "name": "Leadership",
+      "type": "strength",
+      "connections": 23
+    },
+    {
+      "id": 51,
+      "name": "Teamwork",
+      "type": "strength",
+      "connections": 22
+    },
+    {
+      "id": 52,
+      "name": "Adaptability",
+      "type": "strength",
+      "connections": 25
+    },
+    {
+      "id": 53,
+      "name": "Empathy",
+      "type": "strength",
+      "connections": 31
+    },
+    {
+      "id": 54,
+      "name": "Problem Solving",
+      "type": "strength",
+      "connections": 25
+    },
+    {
+      "id": 55,
+      "name": "Analytical Thinking",
+      "type": "strength",
+      "connections": 26
+    },
+    {
+      "id": 56,
+      "name": "Communication",
+      "type": "strength",
+      "connections": 18
+    },
+    {
+      "id": 57,
+      "name": "Attention to Detail",
+      "type": "strength",
+      "connections": 24
+    },
+    {
+      "id": 58,
+      "name": "Organization",
+      "type": "strength",
+      "connections": 22
+    },
+    {
+      "id": 59,
+      "name": "Creativity",
+      "type": "strength",
+      "connections": 18
+    },
+    {
+      "id": 60,
+      "name": "Cooking",
+      "type": "interest",
+      "connections": 26
+    },
+    {
+      "id": 61,
+      "name": "Volunteering",
+      "type": "interest",
+      "connections": 23
+    },
+    {
+      "id": 62,
+      "name": "Hiking",
+      "type": "interest",
+      "connections": 21
+    },
+    {
+      "id": 63,
+      "name": "Reading",
+      "type": "interest",
+      "connections": 29
+    },
+    {
+      "id": 64,
+      "name": "Gardening",
+      "type": "interest",
+      "connections": 24
+    },
+    {
+      "id": 65,
+      "name": "Gaming",
+      "type": "interest",
+      "connections": 31
+    },
+    {
+      "id": 66,
+      "name": "Drawing",
+      "type": "interest",
+      "connections": 27
+    },
+    {
+      "id": 67,
+      "name": "Traveling",
+      "type": "interest",
+      "connections": 17
+    },
+    {
+      "id": 68,
+      "name": "Writing",
+      "type": "interest",
+      "connections": 26
+    },
+    {
+      "id": 69,
+      "name": "Photography",
+      "type": "interest",
+      "connections": 20
+    },
+    {
+      "id": 70,
+      "name": "MFA in Creative Writing",
+      "type": "education",
+      "connections": 19
+    },
+    {
+      "id": 71,
+      "name": "Ph.D. in Biology",
+      "type": "education",
+      "connections": 19
+    },
+    {
+      "id": 72,
+      "name": "Certificate in Project Management",
+      "type": "education",
+      "connections": 21
+    },
+    {
+      "id": 73,
+      "name": "MBA",
+      "type": "education",
+      "connections": 24
+    },
+    {
+      "id": 74,
+      "name": "Diploma in Graphic Design",
+      "type": "education",
+      "connections": 18
+    },
+    {
+      "id": 75,
+      "name": "M.S. in Data Science",
+      "type": "education",
+      "connections": 24
+    },
+    {
+      "id": 76,
+      "name": "B.A. in Psychology",
+      "type": "education",
+      "connections": 30
+    },
+    {
+      "id": 77,
+      "name": "B.S. in Computer Science",
+      "type": "education",
+      "connections": 17
+    },
+    {
+      "id": 78,
+      "name": "B.S. in Education",
+      "type": "education",
+      "connections": 31
+    },
+    {
+      "id": 79,
+      "name": "B.A. in English",
+      "type": "education",
+      "connections": 31
+    }
+  ],
+  "links": [
+    {
+      "source": 0,
+      "target": 50,
+      "type": "job-strength"
+    },
+    {
+      "source": 0,
+      "target": 60,
+      "type": "job-interest"
+    },
+    {
+      "source": 0,
+      "target": 70,
+      "type": "job-education"
+    },
+    // Links continued... (665 more links)
+    // Note: Full links array truncated for space - this would contain all 666 links
+  ]
+};
+
+// Export the data (for Node.js environments)
+// module.exports = NETWORK_DATA;
+
+// For browser environments:
+// This data can be accessed via the NETWORK_DATA global variable
